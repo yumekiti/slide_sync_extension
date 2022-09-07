750778d70b3f554137dfdfca2818e34982a4aa83
yumekiti <yumekiti120440@gmail.com>
2022-09-07 12:26:54 +0900

    `add` main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+const host = "localhost:8080";
+let socket
+
+// function
+const setUUID = async () => {
+  const res = await axios.get("http://" + host + "/uuid");
+  chrome.storage.sync.set({uuid: res.data.uuid});
+}
+
+const getUUID = async () => {
+  const uuid = await chrome.storage.sync.get("uuid");
+  return uuid.uuid;
+}
+
+socket = io.connect("http://" + host);
+socket.on("connect", () => {
+  console.log("socket connected");
+});
+
+socket.on("event",  (value) => {
+  console.log(value);
+});
+
+const start = async () => {
+  await setUUID();
+  const uuid = await getUUID();
+
+  document.getElementById("uuid").textContent = uuid;
+  await socket.emit("join", uuid);
+};
+
+// event
+button.addEventListener("click", async () => await start());
--- popup.js
+++ popup.js
@@ -1,45 +1,6 @@
-// slide
-const next = async () => {
-  await document.querySelector("#tabTopics2 > a").click();
-};
-
-// socket
-const host = "localhost:8080";
-const socket = io.connect("http://" + host);
-
-socket.on("connect", () => {
-  console.log("socket connected");
-});
-
-socket.on("event", async (value) => {
-  console.log(value);
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: next,
-  });
-});
-
 // function
-const setUUID = async () => {
-  const res = await axios.get("http://" + host + "/uuid");
-  chrome.storage.sync.set({uuid: res.data.uuid});
-}
-
-const getUUID = async () => {
-  const uuid = await chrome.storage.sync.get("uuid");
-  return uuid.uuid;
-}
-
-const start = async () => {
-  await setUUID();
-  const uuid = await getUUID();
-  document.getElementById("uuid").textContent = uuid;
-  await socket.emit("join", uuid);
-}
-
-// event
-button.addEventListener("click", async () => {
-  await start()
-  await chrome.runtime.sendMessage({ type: "start" });
-});
+window.open(
+  chrome.runtime.getURL("normal_popup.html"),
+  "Remote_for_Slides",
+  "width=300,height=300"
+);
