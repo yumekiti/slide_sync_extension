ef64502d809b782350fb24e99d3b24645a99ac9b
ゆめきち <68057090+yumekiti@users.noreply.github.com>
2022-10-27 19:22:16 +0900

    `chenge` host

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const host = 'remote-for-slides.up.railway.app';
+const host = 'remote.yumekiti.net';
 
 // function
 const setUUID = async () => {
-  const res = await axios.get('http://' + host + '/uuid');
+  const res = await axios.get('https://' + host + '/uuid');
   chrome.storage.sync.set({ uuid: res.data.uuid });
 };
 
@@ -11,7 +11,7 @@ const getUUID = async () => {
   return uuid.uuid;
 };
 
-const socket = io.connect('http://' + host);
+const socket = io.connect('://' + host);
 socket.on('connect', () => {
   console.log('socket connected');
 });
@@ -50,7 +50,7 @@ const createButtonElement = document.getElementById('createButton');
 const start = async () => {
   await setUUID();
   const uuid = await getUUID();
-  const link = 'http://' + host + '/room/' + uuid;
+  const link = 'https://' + host + '/room/' + uuid;
 
   const qrcodeElement = document.getElementById('qrcode');
   qrcodeElement.innerHTML = '';
